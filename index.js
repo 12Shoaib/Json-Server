bf2540cb423ddbe4408aec3fd77fc3624877ae8c
Mohd Shoaib <Shoaibmohd916@gmail.com>
2023-06-07 20:52:56 +0530

    updated db

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const jsonServer = require("json-server"); // importing json-server library
 const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
+
+server.use(cors())  
 
 server.delete('/deleteAllScenarios', (req, res) => {
     router.db.set('allScenarios', []);
     res.sendStatus(200);
   });
 
-server.use(cors)  
 server.use(middlewares);
 server.use(router);
 
