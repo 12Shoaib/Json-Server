2eaa7404f67df35baf11f1eb0097a76c6a8f075e
Mohd Shoaib <Shoaibmohd916@gmail.com>
2023-06-07 22:00:07 +0530

    adding changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const cors = require('cors');
+const fs = require('fs');
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -9,9 +10,13 @@ const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
 server.use(cors())  
 
 server.delete('/deleteAllScenarios', (req, res) => {
-    router.db.set('allScenarios', []);
-    res.sendStatus(200);
-  });
+  router.db.set('allScenarios', []);
+
+  // Write changes back to JSON file
+  fs.writeFileSync('db.json', JSON.stringify(router.db.getState()));
+
+  res.sendStatus(200);
+});
 
 server.use(middlewares);
 server.use(router);
